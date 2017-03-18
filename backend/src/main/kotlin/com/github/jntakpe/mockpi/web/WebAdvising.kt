8215ce1a3f2e1e5152b071e7547acaf4263cd9a9
jntakpe <jntakpe@gmail.com>
2017-03-18 23:45:44 +0100

    ControllerAdvising support

---
+++ backend/src/main/kotlin/com/github/jntakpe/mockpi/web/WebAdvising.kt
@@ -0,0 +1,15 @@
+package com.github.jntakpe.mockpi.web
+
+import com.github.jntakpe.mockpi.exceptions.IdNotFoundException
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+
+@ControllerAdvice
+class WebAdvising {
+
+    @ExceptionHandler(IdNotFoundException::class)
+    fun handleNotFound(exception: IdNotFoundException) = ResponseEntity(exception.message, HttpStatus.NOT_FOUND)
+
+}
