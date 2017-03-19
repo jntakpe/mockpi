d779e08b585c7cce291ca2050d16a76702c8c290
jntakpe <j.ntakpe@gmail.com>
2017-03-19 20:34:07 +0100

    Equals and hashcode on User

--- backend/src/main/kotlin/com/github/jntakpe/mockpi/domain/User.kt
+++ backend/src/main/kotlin/com/github/jntakpe/mockpi/domain/User.kt
@@ -4,4 +4,15 @@ import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document
-data class User(@Id val login: String, val name: String, val email: String)
+data class User(@Id val login: String, val name: String, val email: String) {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is User) return false
+        return login == other.login
+    }
+
+    override fun hashCode(): Int {
+        return login.hashCode()
+    }
+}
