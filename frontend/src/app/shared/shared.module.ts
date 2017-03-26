15041f75c30677fc67295b056d579bab38be5713
jntakpe <jntakpe@gmail.com>
2017-03-26 15:22:26 +0200

    #2 Adding support for covalent

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -6,10 +6,29 @@ import {ReactiveFormsModule} from '@angular/forms';
 import {MaterialModule, MaterialRootModule} from '@angular/material';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import {SecurityModule} from './security/security.module';
+import {CovalentCoreModule} from '@covalent/core';
 
 @NgModule({
-  imports: [CommonModule, HttpModule, RouterModule, ReactiveFormsModule, MaterialModule, SecurityModule],
-  exports: [CommonModule, HttpModule, RouterModule, ReactiveFormsModule, MaterialRootModule, FlexLayoutModule, SecurityModule]
+  imports: [
+    CommonModule,
+    HttpModule,
+    RouterModule,
+    ReactiveFormsModule,
+    MaterialModule,
+    FlexLayoutModule,
+    CovalentCoreModule.forRoot(),
+    SecurityModule
+  ],
+  exports: [
+    CommonModule,
+    HttpModule,
+    RouterModule,
+    ReactiveFormsModule,
+    MaterialRootModule,
+    FlexLayoutModule,
+    CovalentCoreModule,
+    SecurityModule
+  ]
 })
 export class SharedModule {
 }
