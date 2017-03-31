40c639bc7c9d75552af0cf2d87d57a8ae5a8d404
jntakpe <jntakpe@gmail.com>
2017-03-31 15:24:44 +0200

    Fix typo app-root to mpi-root

--- frontend/e2e/app.po.ts
+++ frontend/e2e/app.po.ts
@@ -1,4 +1,4 @@
-import { browser, element, by } from 'protractor';
+import {browser, by, element} from 'protractor';
 
 export class MockpiPage {
   navigateTo() {
@@ -6,6 +6,6 @@ export class MockpiPage {
   }
 
   getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+    return element(by.css('mpi-root h1')).getText();
   }
 }
